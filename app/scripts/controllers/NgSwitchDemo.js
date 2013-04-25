ddd0cb3083a798310cd1360fc479b6c089756652
Jaime Febres <jaime.febres@gmail.com>
2013-04-25 16:59:07 -0500

    Updated NgSwitchDemo controller to fix jshint warnings regarding whitespaces, tabs and identation

--- app/scripts/controllers/NgSwitchDemo.js
+++ app/scripts/controllers/NgSwitchDemo.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angularE2eApp')
-  .controller('NgSwitchDemoCtrl', function ($scope) {    
-	$scope.options = ['1st','2nd','3rd'];
-	$scope.selection = $scope.options[0];
-  });
+	.controller('NgSwitchDemoCtrl', function($scope){
+		$scope.options = ['1st','2nd','3rd'];
+		$scope.selection = $scope.options[0];
+	});
