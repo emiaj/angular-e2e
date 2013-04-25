de53308abb549107583071a7a3b883c32633ad69
Jaime Febres <jaime.febres@gmail.com>
2013-04-25 15:59:09 -0500

    [NgSwitchDemoCtrl] Unit test: green

--- app/scripts/controllers/NgSwitchDemo.js
+++ app/scripts/controllers/NgSwitchDemo.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('angularE2eApp')
-  .controller('NgSwitchDemoCtrl', function ($scope) {
-
+  .controller('NgSwitchDemoCtrl', function ($scope) {    
+	$scope.options = ['1st','2nd','3rd'];
+	$scope.selection = $scope.options[0];
   });
